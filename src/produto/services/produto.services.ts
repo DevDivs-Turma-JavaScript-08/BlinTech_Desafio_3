f170cb7e2b7bb99a21abc4db19255ac63439e6d2
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-09-18 12:43:05 -0300

    fix: corrige duplicidade de cadastro de produto

--- src/produto/services/produto.services.ts
+++ src/produto/services/produto.services.ts
@@ -1,104 +1,84 @@
 import { CategoriaService } from './../../categoria/services/categoria.services';
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { DeleteResult, Like, Repository } from "typeorm";
-import { Produto } from "../entities/produto.entity";
-
-
-
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Like, Repository } from 'typeorm';
+import { Produto } from '../entities/produto.entity';
 
 @Injectable()
 export class ProdutoService {
+  constructor(
+    @InjectRepository(Produto)
+    private produtoRepository: Repository<Produto>,
+    private categoriaService: CategoriaService,
+  ) {}
 
-    constructor(
-        @InjectRepository(Produto)
-        private produtoRepository: Repository<Produto>, 
-        private categoriaService: CategoriaService,
+  async findAll(): Promise<Produto[]> {
+    return await this.produtoRepository.find({
+      relations: {
+        categoria: true,
+        usuario: true,
+      },
+    });
+  }
 
-    ) { }
+  async findById(id: number): Promise<Produto> {
+    const produto = await this.produtoRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        categoria: true,
+        usuario: true,
+      },
+    });
 
-    async findAll(): Promise<Produto[]> {
-        return await this.produtoRepository.find({
-            relations: {
-                categoria: true,
-                usuario: true,
-            }
-        });
-    }
+    if (!produto)
+      throw new HttpException('Produto não encontrado', HttpStatus.NOT_FOUND);
 
-    async findById(id: number): Promise<Produto> {
+    return produto;
+  }
 
-        const produto = await this.produtoRepository.findOne({
-            where: {
-                id
-            },
-             relations: {
-                categoria: true,
-                usuario: true,
-            }
-        })
+  async findAllByNome(nomeProduto: string): Promise<Produto[]> {
+    return await this.produtoRepository.find({
+      where: {
+        nomeProduto: Like(`%${nomeProduto}%`),
+      },
+      relations: {
+        categoria: true,
+        usuario: true,
+      },
+    });
+  }
 
-        if (!produto) throw new HttpException('Produto não encontrado', HttpStatus.NOT_FOUND)
-        
-        return produto;
-    }
+  async calcularValores(produto: Produto): Promise<Produto> {
+    let porcentagemBase = 1.3;
 
-    async findAllByNome(nomeProduto: string): Promise<Produto[]> {
-        return await this.produtoRepository.find({
-            where: {
-                nomeProduto: Like(`%${nomeProduto}%`)
-            },
-             relations: {
-                categoria: true,
-                usuario: true,
-            }
-        })
-    }
+    if (produto.cobertura === 'premium') porcentagemBase = 1.5;
+    else if (produto.cobertura === 'intermediario') porcentagemBase = 1.2;
 
-    async createValorFinal(produto: Produto): Promise<Produto> {
-        let porcentagemBase = 1.3
+    produto.valorSeguro = produto.valorProduto * porcentagemBase;
+    if (produto.tempoUso >= 3) produto.valorSeguro *= 0.7;
 
-        if(produto.cobertura == "premium") {
-          porcentagemBase = 1.5
-        } else if(produto.cobertura == "intermediario") {
-          porcentagemBase = 1.2
-        }
+    produto.premioMensal = produto.valorSeguro * 0.005;
+    return produto;
+  }
 
-        produto.valorSeguro = produto.valorProduto*porcentagemBase
+  async create(produto: Produto): Promise<Produto> {
+    this.calcularValores(produto);
+    return await this.produtoRepository.save(produto);
+  }
 
-        if(produto.tempoUso >= 3) produto.valorSeguro = produto.valorSeguro * 0.7
+  async update(produto: Produto): Promise<Produto> {
+    await this.findById(produto.id);
+    await this.categoriaService.findById(produto.categoria.id);
 
-        return await this.produtoRepository.save(produto);
-    }
+    this.calcularValores(produto);
+    return await this.produtoRepository.save(produto);
+  }
 
-    async create(produto: Produto): Promise<Produto> {
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
 
-        this.createValorFinal(produto)
-
-        produto.premioMensal = produto.valorSeguro * 0.005
-
-        return await this.produtoRepository.save(produto)
-    }
-
-    async update(produto: Produto): Promise<Produto> {
-
-        const postagem_id = await this.findById(produto.id);
-        await this.categoriaService.findById(produto.categoria.id);
-        if (!postagem_id) {
-            throw new HttpException("Produto não encontrado", HttpStatus.NOT_FOUND);
-        }
-
-        this.createValorFinal(produto);
-
-        produto.premioMensal = produto.valorSeguro * 0.005;
-        
-        return await this.produtoRepository.save(produto);
-    }
-
-    async delete (id: number): Promise < DeleteResult > {
-        await this.findById(id)
-
-        return await this.produtoRepository.delete(id)
-}
-
+    return await this.produtoRepository.delete(id);
+  }
 }
